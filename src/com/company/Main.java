22120058f03f8f3a53e220c0624aaad65ddb676d
srikarn <srikarnanduri628@gmail.com>
2018-07-12 12:04:13 +0530

    2st commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     System.out.println("this is first statement");
+    System.out.println("this is second statement");
     }
 }
